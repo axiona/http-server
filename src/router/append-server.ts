c67cedf418e088a3a369c7770855560ff3d7d479
dikac <rigel234@yahoo.com>
2022-08-07 02:35:54 +0700

    ref: improve type

--- src/router/append-server.ts
+++ src/router/append-server.ts
@@ -1,7 +1,10 @@
 import Router from './router';
 import Server from '../server/server';
+import Standard from "./standard";
 
-export default function AppendServer<Type extends Router>(server : Server, router: Type) : Type {
+export default function AppendServer(server : Server) : Router;
+export default function AppendServer<Type extends Router>(server : Server, router: Type) : Type;
+export default function AppendServer<Type extends Router>(server : Server, router: Type|Router = new Standard()) : Type|Router {
 
     server.koa.use((context, next) => {
 
