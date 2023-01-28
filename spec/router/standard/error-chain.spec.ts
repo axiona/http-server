1b769241b31805092b99751af9dbf3fa86276de7
dikac <rigel234@yahoo.com>
2023-01-29 03:35:15 +0700

    test: add error-chain

---
+++ spec/router/standard/error-chain.spec.ts
@@ -0,0 +1,82 @@
+import Router from '../../../dist/router/middleware';
+import Server from '../../server';
+import BindToServer from '../../../dist/router/append-server';
+import Axios, {AxiosResponse} from 'axios';
+
+it('force console log', () => { spyOn(console, 'log').and.callThrough();});
+
+describe('basic', function () {
+
+    const server = Server();
+
+    beforeAll(()=>server.open());
+    afterAll(()=>server.close());
+
+    let response : AxiosResponse<{message:string}>;
+
+    const router =  BindToServer(server, Router());
+
+    let called : string[] = [];
+
+    router.catch(function (ctx, error) {
+
+        called.push('Catch 1');
+        ctx.response.body = {message:error.message};
+
+    }).catch(function (ctx, error) {
+
+        called.push('Catch 2');
+        throw error;
+
+    }).extends(
+        r=>r.catch(function (ctx, error) {
+
+            called.push('Catch 3');
+            throw error;
+
+        }).catch(function (ctx, error) {
+
+            called.push('Catch 4');
+            throw error;
+
+        })
+    ).extends(
+        r=>r.catch(function (ctx, error) {
+
+            called.push('Catch 5');
+            throw error;
+
+        }).catch(function (ctx, error) {
+
+            called.push('Catch 6');
+            throw error;
+
+        })
+    ).next(function (context) {
+
+        called.push('Throw 1');
+        throw new Error('Throw 1');
+    });
+
+    it('send request', function (done) {
+
+        Axios.post(`http://localhost:${server.config.port}`)
+            .then(r=>response=r)
+            .catch(fail)
+            .finally(done);
+    });
+
+    it('assert value', function () {
+
+        expect(response.data).toEqual({message:'Throw 1'});
+        expect(called).toEqual([
+            'Throw 1',
+            'Catch 6',
+            'Catch 5',
+            'Catch 4',
+            'Catch 3',
+            'Catch 2',
+            'Catch 1'
+        ]);
+    });
+});
