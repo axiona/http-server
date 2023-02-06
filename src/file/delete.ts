dca3194d6a2e57ed7fd5e5b934dee4d445ee33d6
dikac <rigel234@yahoo.com>
2023-02-07 05:06:11 +0700

    ref: rename to normal test

--- src/file/delete.ts
+++ src/file/delete.ts
@@ -2,9 +2,9 @@ import Fs from "fs";
 import NoOp from "../../../function/dist/no-op";
 import {File as FormidableFile} from "formidable";
 
-export default function Delete(file : FormidableFile, errorHandler: (error)=>void = NoOp) : Promise<void> {
+export default function Delete(file : FormidableFile, errorHandler: (error)=>void = NoOp) : Promise<FormidableFile> {
 
     const absolute = file.filepath;
 
-    return Fs.promises.unlink(absolute).catch(errorHandler);
+    return Fs.promises.unlink(absolute).catch(errorHandler).then(() => file);
 }
